200f92f6f0a98aee309021893e0f1292d843177e
Rafael <therafaelfirme@gmail.com>
2025-09-08 12:12:31 -1000

    finished call form

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,98 @@
-import { useState, useEffect} from 'react'
-import {vapi, startAssistant, stopAssistant} from "./ai"
+import { useState, useEffect } from "react";
+import { vapi, startAssistant, stopAssistant } from "./ai";
 
 function App() {
+  //useState stores a value that will be changed
+  const [started, setStarted] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [assistantIsSpeaking, setAssistantIsSpeaking] = useState(false);
+  const [volumeLevel, setVolumeLevel] = useState(0);
+  const [callId, setCallId] = useState("");
+  const [callResult, setCallResult] = useState(null);
+  const [loadingResult, setLoadingResult] = useState(false);
+
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
   useEffect(() => {
-    startAssistant()
-  }, [])
+    vapi
+      .on("call-start", () => {
+        setLoading(false);
+        setStarted(true);
+      })
+      .on("call-end", () => {
+        setLoading(false);
+        setStarted(false);
+      })
+      .on("speech-start", () => {
+        setAssistantIsSpeaking(true);
+      })
+      .on("speech-end", () => {
+        setAssistantIsSpeaking(false);
+      })
+      .on("volume-level", (level) => {
+        setVolumeLevel(level);
+      }); // later use could have .on("message") for live transcription
+  }, []);
+
+  // sets the event to the target value
+  const handleInputChange = (setter) => (event) => {
+    setter(event.target.value);
+      };
+  
+  const handleStart = async () => {
+    setLoading(true)
+    const data = await startAssistant(firstName, lastName, phoneNumber)
+    setCallId(data.id)
+  }
+
+  const handleStop = () => {
+    stopAssistant()
+    // get call details
+  }
+  const showForm = !loading && !started && !loadingResult && !callResult;
+  const allFieldsFilled = firstName && lastName && phoneNumber;
 
   return (
-    <>
-      
-    </>
-  )
+    <div className="app-container">
+      {showForm && (
+        <>
+          <h1> Contact Details (Required) </h1>
+          <input
+            type="text"
+            placeholder="First Name"
+            value={firstName}
+            className="input-field"
+            onChange={handleInputChange(setFirstName)}
+          />
+          <input
+            type="text"
+            placeholder="Last Name"
+            value={lastName}
+            className="input-field"
+            onChange={handleInputChange(setLastName)}
+          />
+          <input
+            type="tel"
+            placeholder="Phone Number "
+            value={phoneNumber}
+            className="input-field"
+            onChange={handleInputChange(setPhoneNumber)}
+          />
+          {!started && (
+            <button
+              onClick={handleStart}
+              disabled={!allFieldsFilled}
+              className="button"
+            >
+              Start Storyteller Call
+            </button>
+          )}
+        </>
+      )}
+      {(loading || loadingResult) && <div className="loading"></div>  }
+    </div>
+  );
 }
 
-export default App
+export default App;
